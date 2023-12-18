5184d318b928a21d988f3aaf821bc1d4f50eb811
Kasualix <670067575@qq.com>
2023-12-18 11:44:36 +0800

    1182

--- src/main/java/com/teampotato/francium/mixin/common/RandomClinitMixin.java
+++ src/main/java/com/teampotato/francium/mixin/common/RandomClinitMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.util.Mth;
 import net.minecraft.util.datafix.fixes.EntityZombieVillagerTypeFix;
 import net.minecraft.world.Containers;
 import net.minecraft.world.entity.ai.sensing.Sensor;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.entity.DispenserBlockEntity;
 import net.minecraft.world.level.block.entity.EnchantmentTableBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-@Mixin({Item.class, Sensor.class, Containers.class, ServerLoginPacketListenerImpl.class, DispenserBlockEntity.class, EnchantmentTableBlockEntity.class, EntityZombieVillagerTypeFix.class, Mth.class})
+@Mixin({Sensor.class, Containers.class, ServerLoginPacketListenerImpl.class, DispenserBlockEntity.class, EnchantmentTableBlockEntity.class, EntityZombieVillagerTypeFix.class, Mth.class})
 public abstract class RandomClinitMixin {
     @Redirect(method = "<clinit>", at = @At(value = "NEW", target = "()Ljava/util/Random;", remap = false))
     private static Random useThreadLocalRandom() {
--- src/main/java/com/teampotato/francium/mixin/common/RandomInitMixin.java
+++ src/main/java/com/teampotato/francium/mixin/common/RandomInitMixin.java
@@ -2,7 +2,6 @@ package com.teampotato.francium.mixin.common;
 
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.ai.behavior.WeightedList;
 import net.minecraft.world.entity.npc.WanderingTraderSpawner;
 import net.minecraft.world.entity.raid.Raid;
 import net.minecraft.world.level.Explosion;
@@ -15,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-@Mixin({Entity.class, Level.class, MinecraftServer.class, Block.class, WanderingTraderSpawner.class, Raid.class, Explosion.class, WeightedList.class})
+@Mixin({Entity.class, Level.class, MinecraftServer.class, Block.class, WanderingTraderSpawner.class, Raid.class, Explosion.class})
 public abstract class RandomInitMixin {
     @Redirect(method = "<init>*", at = @At(value = "NEW", target = "()Ljava/util/Random;", remap = false))
     private Random useThreadLocalRandom() {
